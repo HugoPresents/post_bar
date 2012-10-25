507be7278eeed4d3c9fc78d4a73f5ebb3e95ed83
Rabbit_52 <rabbitzhang52@gmail.com>
2012-10-25 15:51:57 +0800

    update backstage

--- config/urls.py
+++ config/urls.py
@@ -53,6 +53,8 @@ urls = (
     '/node/unfav/(\w+)', pre_fix + 'node.unfav',
     # 后台管理
     '/admin', pre_fix + 'admin.index',
+    # 分类编辑
+    '/admin/cat/(\w+)', pre_fix + 'admin.cat',
     # 其他
     '/.*', pre_fix + 'index.index'
 )
--- controllers/admin.py
+++ controllers/admin.py
@@ -10,11 +10,13 @@ from models.user_model import *
 from models.comment_model import *
 from libraries.crumb import Crumb
 
-class index:
-    
+class admin:
     def __init__(self):
         if session.user_id != 1:
             raise web.SeeOther('/')
+
+class index(admin):
+    
     crumb = Crumb()
     
     def GET(self):
@@ -28,4 +30,14 @@ class index:
             node_total = node_model().count_table({'category_id':cat.id})
             cats.append({'cat':cat, 'node_total':node_total})
         self.crumb.append('后台')
-        return admin_render.index('管理', site_count, cats, self.crumb.output())
+        return admin_render.index('后台', site_count, cats, self.crumb.output())
+
+class cat(admin):
+    
+    crumb = Crumb()
+    
+    def GET(self, cat_name):
+        cat = cat_model().get_one({'name':cat_name})
+        if cat is None:
+            self.crumb.append('分类不存在')
+            return admin_render.index('分类不存在', self.crumb.output())
